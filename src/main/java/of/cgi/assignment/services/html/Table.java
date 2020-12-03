25b9e51799b121464e4fb4e271057669f7895772
Samiron Paul <samironpaul@gmail.com>
2020-12-03 10:01:30 -0400

    Html Table class created to render a table from data

---
+++ src/main/java/of/cgi/assignment/services/html/Table.java
@@ -0,0 +1,43 @@
+package of.cgi.assignment.services.html;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Table {
+	private final List<String> columns = new ArrayList<>();
+	private final List<List<String>> rows = new ArrayList<>();
+
+	public Table(String... headers) {
+		this.columns.addAll(Arrays.asList(headers));
+	}
+
+	public Table addRow(List<String> values) {
+		rows.add(values);
+		return this;
+	}
+
+	public String toHtml() {
+		StringBuilder tableBuilder = new StringBuilder();
+		tableBuilder.append("<table>");
+
+		// Add table header
+		tableBuilder.append("<tr>");
+		for (String c : columns) {
+			tableBuilder.append("<th>").append(c).append("</th>");
+		}
+		tableBuilder.append("</tr>");
+
+		// Add table rows
+		for (List<String> values : rows) {
+			tableBuilder.append("<tr>");
+			for (String c : values) {
+				tableBuilder.append("<td>").append(c).append("</td>");
+			}
+			tableBuilder.append("</tr>");
+		}
+
+		tableBuilder.append("</table>");
+		return tableBuilder.toString();
+	}
+}
---
+++ src/test/java/of/cgi/assignment/services/html/TableTest.java
@@ -0,0 +1,24 @@
+package of.cgi.assignment.services.html;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class TableTest {
+
+	@Test
+	public void toHtml(){
+		Table t = new Table("First Name", "Last Name");
+		t.addRow(Arrays.asList("Samiron", "Paul"));
+		t.addRow(Arrays.asList("Paul", "Mathew"));
+		t.addRow(Arrays.asList("Another", "Person"));
+
+		String response = t.toHtml();
+		assertThat(response, is(equalTo("<table><tr><th>First Name</th><th>Last Name</th></tr><tr><td>Samiron</td><td>Paul</td></tr><tr><td>Paul</td><td>Mathew</td></tr><tr><td>Another</td><td>Person</td></tr></table>")));
+	}
+
+}
