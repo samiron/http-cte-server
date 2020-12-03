feb999d712209cc95079e09c694cc2525b58edd1
Samiron Paul <samironpaul@gmail.com>
2020-12-03 09:07:16 -0400

    Router is picking up a service, HttpService is delegating to method.

--- src/main/java/of/cgi/assignment/kernel/Router.java
+++ src/main/java/of/cgi/assignment/kernel/Router.java
@@ -1,32 +1,58 @@
 package of.cgi.assignment.kernel;
 
+import of.cgi.assignment.http.exception.NotFoundException;
 import of.cgi.assignment.http.request.HttpRequest;
 import of.cgi.assignment.services.Service;
-import of.cgi.assignment.services.WebService;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+@SuppressWarnings("unchecked")
 public class Router {
 	private static final Router router = new Router();
-	Map<String, Service> routes = new HashMap<>();
 
-	private Router(){
-		routes.put("webService", new WebService());
+	private final Map<String, String> directories = new ConcurrentHashMap<>();
+	private final Map<String, Service> routes = new ConcurrentHashMap<>();
+
+	private Router() {
+
+		Map<String, String> routeConfig = Configuration.get().getRoutes();
+		for (Map.Entry<String, String> routeEntry :
+				routeConfig.entrySet()) {
+			try {
+				Class<Service> clazz;
+				clazz = (Class<Service>) Class.forName(routeEntry.getValue());
+				Service service;
+				service = clazz.newInstance();
+				this.routes.put(routeEntry.getKey(), service);
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Failed to initialize route", e);
+			}
+		}
+
+		this.directories.putAll(Configuration.get().getDirectories());
 	}
 
 	public static Router get() {
-		synchronized (router){
+		synchronized (router) {
 			return router;
 		}
 	}
 
-	public ServiceHandler<Service> getHandler(HttpRequest request) throws NoSuchMethodException {
-		synchronized (router){
-			Service serviceClass = routes.get("webService");
-			Method method = serviceClass.getClass().getDeclaredMethod("doGet", HttpRequest.class);
-			return new ServiceHandler<>(serviceClass, method);
+	public ServiceHandler<Service> getHandler(HttpRequest request) throws NoSuchMethodException, NotFoundException {
+		String path = request.getPath();
+		String[] partParts = path.split("/");
+		String firstPart = partParts[0];
+		if(firstPart.isEmpty()){
+			firstPart = "web";
 		}
+
+		Service serviceClass = routes.get(firstPart);
+		Method method;
+		method = serviceClass.getClass().getMethod("serve", HttpRequest.class);
+		return new ServiceHandler<>(serviceClass, method);
 	}
+
 }
--- src/main/java/of/cgi/assignment/services/HttpService.java
+++ src/main/java/of/cgi/assignment/services/HttpService.java
@@ -1,6 +1,8 @@
 package of.cgi.assignment.services;
 
+import of.cgi.assignment.http.HttpMethod;
 import of.cgi.assignment.http.exception.HttpException;
+import of.cgi.assignment.http.exception.MethodNotAllowedException;
 import of.cgi.assignment.http.request.HttpRequest;
 import of.cgi.assignment.http.response.HttpResponse;
 
@@ -8,7 +10,13 @@ public abstract class HttpService implements Service {
 
 	@Override
 	public HttpResponse serve(HttpRequest request) throws HttpException {
-		return null;
+		if (request.getMethod().equals(HttpMethod.GET)) {
+			return doGet(request);
+		} else if (request.getMethod().equals(HttpMethod.POST)) {
+			return doPost(request);
+		} else {
+			throw new MethodNotAllowedException(request.getMethod() + " is not allowed");
+		}
 	}
 
 	abstract public HttpResponse doGet(HttpRequest request) throws HttpException;
